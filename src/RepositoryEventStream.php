0b955130f8449d7bc443407f46eebcf849069494
Josh Butts <josh@joshbutts.com>
2017-02-17 10:45:19 -0600

    initial commit

---
+++ src/RepositoryEventStream.php
@@ -0,0 +1,81 @@
+<?php
+namespace GithubRepoEvents;
+
+use Resty\Resty;
+
+class RepositoryEventStream implements \IteratorAggregate
+{
+    const MAX_PAGES = 10;
+    const PER_PAGE = 30;
+
+    protected $apiKey;
+    protected $etag;
+    protected $repo;
+    protected $currentPage = 1;
+    protected $itemOnPage = 1;
+    protected $iteratedEvents = [];
+    protected $hasNewEvents = true;
+
+    public function __construct($repo, $apiKey = null)
+    {
+        $this->apiKey = $apiKey;
+        $this->repo = $repo;
+    }
+
+    public function setEtag($etag)
+    {
+        $this->etag = $etag;
+    }
+
+    public function getEtag()
+    {
+        return $this->etag;
+    }
+
+    public function getIterator()
+    {
+        while ($this->currentPage <= self::MAX_PAGES) {
+            $events = $this->fetchPage($this->currentPage);
+            $numEvents = count($events);
+            if ($numEvents) {
+                foreach ($events as $event) {
+                    yield $event;
+                }
+                if ($numEvents == self::PER_PAGE) {
+                    $this->currentPage++;
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+        }
+    }
+
+
+    protected function fetchPage($page)
+    {
+        $resty = new Resty();
+        $resty->setBaseUrl('https://api.github.com');
+
+        $path = "/repos/$this->repo/events";
+
+        $query = ["page" => $page];
+        if ($this->apiKey) {
+            $query["access_token"] = $this->apiKey;
+        }
+
+        $headers = [];
+        if ($this->etag) {
+            $headers["ETag"] = $this->etag;
+        }
+
+        $response = $resty->get($path, $query, $headers);
+
+        if (isset($response["headers"]["ETag"])) {
+            $this->etag = $response["headers"]["ETag"];
+        }
+
+        return $response["body"];
+    }
+}
